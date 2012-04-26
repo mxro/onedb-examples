edcae8f53411b7a53090e535d893dbb19c1d8859
Max <Max@Max-PC>
2012-04-26 12:30:31 +1200

    committing example from article

---
+++ src/main/java/one/examples/z_articles/A_IntroducingOnedb.java
@@ -0,0 +1,24 @@
+package one.examples.z_articles;
+
+import one.client.jre.OneJre;
+import one.common.One;
+import one.core.domain.OneClient;
+import one.core.dsl.callbacks.When;
+import one.core.nodes.OneNode;
+
+public class A_IntroducingOnedb {
+	
+	public static void main(String[] args) {
+		OneJre.init("[your API key here]");
+		One.createRealm("foo").and(new When.RealmCreated() {
+			
+			@Override
+			public void thenDo(OneClient client, OneNode root, String secret, String partnerSecret) {
+				One.append("bar").to(root).in(client);
+				
+				System.out.println("Created "+root+":"+secret);
+			}
+		});
+	}
+	
+}
