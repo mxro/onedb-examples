b72f188422857183e090b0788f35a0e2526ec883
Max <Max@Max-PC>
2012-04-26 13:18:55 +1200

    adding another example for Introducing onedb article

--- src/main/java/one/examples/z_articles/A_IntroducingOnedb.java
+++ src/main/java/one/examples/z_articles/A_IntroducingOnedb.java
@@ -7,18 +7,19 @@ import one.core.dsl.callbacks.When;
 import one.core.nodes.OneNode;
 
 public class A_IntroducingOnedb {
-	
-	public static void main(String[] args) {
-		OneJre.init("[your API key here]");
-		One.createRealm("foo").and(new When.RealmCreated() {
-			
-			@Override
-			public void thenDo(OneClient client, OneNode root, String secret, String partnerSecret) {
-				One.append("bar").to(root).in(client);
-				
-				System.out.println("Created "+root+":"+secret);
-			}
-		});
-	}
-	
+
+    public static void main(String[] args) {
+        OneJre.init("[your API key here]");
+        One.createRealm("foo").and(new When.RealmCreated() {
+
+            @Override
+            public void thenDo(OneClient client, OneNode root, String secret,
+                    String partnerSecret) {
+                One.append("bar").to(root).in(client);
+
+                System.out.println("Created " + root + ":" + secret);
+            }
+        });
+    }
+
 }
